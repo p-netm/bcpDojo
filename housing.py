a63c911b53dadcd3fd6ea7722970b6a185aaac7a
= <pmuriuki98@gmail.com>
2017-05-29 01:12:15 +0300

    modify set_person_id method

--- housing.py
+++ housing.py
@@ -1,3 +1,5 @@
+import random
+
 class Room(object):
     max_space = 0
     name = ""
@@ -40,17 +42,24 @@ class Person(object):
     def set_person_id(self):
         """ i need a unique id every time; so i thought of using the date and
          time as string alphanum that make a unique id every instance"""
-        bool_counter = True
-        while bool_counter:
-            print('Please type in your id(q to quit): ')
-            input_id = str(input())
-            if input_id .isdigit() and (7 == len(input_id) or len(input_id) == 8):
-                bool_counter = False
-                return input_id
-            elif input_id == 'q':
-                raise TypeError("\nid not assigned; person not created")
-            else:
-                print("The id should be numeric with either 8 or 7 digits")
+        # proposed change: i think we can create an id for each person and then the person can view
+        # it through display before reallocation --> the problem with this approach is that it will always add
+        # add a person regardless whether they are the same person and actually give them different id's
+
+
+        # bool_counter = False
+        input_id = random.randrange(0000000, 99999999)
+        return input_id
+        # while bool_counter:
+        #     print('Please type in your id(q to quit): ')
+        #     input_id = str(input())
+        #     if input_id .isdigit() and (7 == len(input_id) or len(input_id) == 8):
+        #         bool_counter = False
+        #         return input_id
+        #     elif input_id == 'q':
+        #         raise TypeError("\nid not assigned; person not created")
+        #     else:
+        #         print("The id should be numeric with either 8 or 7 digits")
 
     def get_type(self):
         pass
