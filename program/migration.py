96129729552878e7b262115682421cf4a97e57ab
= <pmuriuki98@gmail.com>
2017-06-16 13:37:24 +0300

    rework database schema

--- program/migration.py
+++ program/migration.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Enum
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
@@ -6,42 +6,30 @@ base = declarative_base()
 
 
 class Person(base):
-    __tablename__ = "Persons"
+    __tablename__ = "persons"
 
-    person_id = Column(String, primary_key=True)
-    full_name = Column(String)
-    occupation = Column(String)  # check and see if this can be translated to enum type ORMS
-    office_name = Column(String, ForeignKey('Offices.office_name'), nullable=True)
-    living_space_name = Column(String, ForeignKey('Living_spaces.living_space_name'), nullable=True)
+    person_id = Column(String(10), primary_key=True)
+    full_name = Column(String(100))
+    occupation = Column(Enum('Fellow', 'Staff'))  # check and see if this can be translated to enum type ORMS
+    office_name = Column(String, ForeignKey('rooms.room_name'), nullable=True)
+    space_name = Column(String, ForeignKey('rooms.room_name'), nullable=True)
 
-    def __init__(self, id, f_name, occupation, office="None", space="None"):
-        self.person_id = id
-        self.full_name = f_name
-        self.occupation = occupation
-        self.office_name = office
-        self.living_space_name = space
+    person1 = relationship('Room', back_populates='persons')
+    person2 = relationship('Room', back_populates='persons')
 
-
-class Office(base):
-    __tablename__ = "Offices"
-
-    office_name = Column(String, primary_key=True)
-
-    def __init__(self, office_name):
+    def __init__(self, person_id, full_name, occ, office_name='None', space_name= "None"):
+        self.person_id = person_id
+        self.full_name = full_name
+        self.occupation = occ
         self.office_name = office_name
+        self.space_name = space_name
 
 
-class Living_space(base):
-    __tablename__ = "Living_spaces"
+class Room(base):
+    __tablename__ = "rooms"
 
-    living_space_name = Column(String, primary_key=True)
+    room_name = Column(String, primary_key=True)
+    room_type = Column(Enum('office', 'living_space'), nullable=False)
+    occupants = Column(Integer, nullable=False)
 
-    def __init__(self, space_name):
-        self.living_space_name = space_name
-
-
-
-
-def retrieve_office_rooms():
-    session.query(Office).all()
 
